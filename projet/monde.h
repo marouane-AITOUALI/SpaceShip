40164b6e773b8993ffa9241d026c291802522bb4
Marouane AIT OUALI <marouane.ait-ouali5@etu.univ-lorraine.fr>
2021-05-26 21:08:08 +0200

    niveau3

--- projet/monde.h
+++ projet/monde.h
@@ -47,26 +47,26 @@ void update_walls(world_t *world);
 
 
 /**
- * \brief La fonction initialise les données du monde du jeu
- * \param world les données du monde
- */
+* \brief La fonction initialise les données du monde du jeu
+* \param world les données du monde
+*/
 
 void init_data(world_t * world);
 
 
 /**
- * \brief La fonction met à jour les données en tenant compte de la physique du monde
- * \param world les données du monde
- */
+* \brief La fonction met à jour les données en tenant compte de la physique du monde
+* \param world les données du monde
+*/
 
 void update_data(world_t *world);
 
 
 /**
- * \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
- * \param event paramètre qui contient les événements
- * \param world les données du monde
- */
+* \brief La fonction gère les évènements ayant eu lieu et qui n'ont pas encore été traités
+* \param event paramètre qui contient les événements
+* \param world les données du monde
+*/
 
 void handle_events(SDL_Event *event,world_t *world);
 
